91c63bc7404ca3ad86f8fca7accba7956631bf6a
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-03-02 00:32:56 +0530

    add cs

--- login/src/components/loginComponents/Button/Button.jsx
+++ login/src/components/loginComponents/Button/Button.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-function Button({ type, btnname, isDisabled, onClick }) {
+function Button({ type, btnname, isDisabled }) {
   return (
     <button
       className={`border border-gray-300 rounded-lg p-2 font-bold mt-6 ${
-        isDisabled()
+        isDisabled
           ? "bg-gray-200 text-gray-500 cursor-not-allowed"
           : "bg-blue-500 text-white hover:bg-blue-600 cursor-pointer"
       } `}
       type={type}
-      disabled={isDisabled()}
-      onClick={onClick}
+      disabled={isDisabled}
     >
       {btnname}
     </button>
