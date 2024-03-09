5fd4027ab5dded8333dadfa6395c702aee4447bd
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-03-09 12:36:42 +0530

    make routes

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "./components";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Layout from "./Layout";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage />,
+    },
+    {
+      path: "/layout",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Dashboard />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <>
-      <LoginPage />
+      <RouterProvider router={router} />
     </>
   );
 }
