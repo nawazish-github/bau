f286556f9304c171ed36f209a287ae5bad71528b
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-03-12 15:03:36 +0530

    chicklet size will be same. chicklet color will be different. in a row there will be three chicklet only. 500 internal server error will be resolved.

--- login/src/components/Dashboard/Dashboard.jsx
+++ login/src/components/Dashboard/Dashboard.jsx
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Dashboard() {
+  const [services, setServices] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3333/bau/services")
+      .then((response) => {
+        // console.log("the respose is:", response);
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Server response was not ok");
+        }
+      })
+      .then((data) => {
+        // console.log("the data is:", data);
+        if (data.status === "success") {
+          setServices(data.services);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
-      <div className="flex justify-center items-center h-24 bg-yellow-100">
-        <div className="p-8 font-bold text-xl">Dashboard</div>
+      <div className="flex justify-center items-center h-24 bg-yellow-200">
+        <div className="font-bold text-xl">Dashboard</div>
       </div>
       <div className="w-full flex justify-evenly items-center mt-16">
-        <div>
-          <button
-            type="button"
-            className="bg-red-300 rounded-xl p-8 pl-12 pr-12 shadow-lg shadow-red-500 font-bold"
-          >
-            Sales
-          </button>
-        </div>
-        <div>
-          <button
-            type="button"
-            className="bg-green-300 rounded-xl p-8 shadow-lg shadow-green-500 font-bold"
-          >
-            Purchases
-          </button>
-        </div>
-        <div>
-          <button
-            type="button"
-            className="bg-blue-300 rounded-xl p-8 shadow-lg shadow-blue-500 font-bold"
-          >
-            Inventory
-          </button>
-        </div>
+        {services &&
+          services.map((services, index) => (
+            <div key={index}>
+              <button
+                type="button"
+                className="bg-green-300 rounded-xl p-8 pl-12 pr-12 shadow-lg shadow-green-500 font-bold"
+              >
+                {services[0].toUpperCase() + services.slice(1)}
+              </button>
+            </div>
+          ))}
       </div>
     </>
   );
