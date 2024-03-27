efd009e76e965ed67bfcfca8fc87d038a06cd90f
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-03-28 00:05:09 +0530

    changes header height

--- login/src/components/Header/Header.jsx
+++ login/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
 
   return (
     <>
-      <div className=" bg-fuchsia-300 flex justify-center items-center h-16">
-        <div className="font-bold text-4xl  ml-96 pl-56 ">BAU</div>
+      <div className=" bg-fuchsia-400 flex justify-center items-center w-full h-14">
+        <div className="font-bold text-4xl  ml-96 pl-60 ">BAU</div>
         <div className="pl-56 ml-72 ">
           Date: {currentDate.toLocaleDateString()}
         </div>
