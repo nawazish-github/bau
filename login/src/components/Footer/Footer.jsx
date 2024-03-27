ebf419786b0800d7cbcdf524ee5f7162e952a4c0
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-03-28 00:04:29 +0530

    add footer

---
+++ login/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="bg-black w-full h-12 flex justify-center items-center">
+        <div className="font-semibold text-2xl text-white">Footer</div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
