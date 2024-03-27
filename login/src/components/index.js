122b05d74b06c049581d7f2489b31929d2178c3b
MD SAMIULLAH <sami.mohd29@gmail.com>
2024-03-28 00:06:48 +0530

    add components path

--- login/src/components/index.js
+++ login/src/components/index.js
@@ -3,5 +3,7 @@ import InputField from "./loginComponents/InputField/InputField";
 import Button from "./loginComponents/Button/Button";
 import Dashboard from "./Dashboard/Dashboard";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import OutletTitle from "./OutletTitle";
 
-export { LoginPage, InputField, Button, Dashboard, Header };
+export { LoginPage, InputField, Button, Dashboard, Header, Footer, OutletTitle };
